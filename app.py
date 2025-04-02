e458864f2e4f47f989614a2953a382f2a9b55715
saurabhgmr <00078989@gmrgroup.in>
2025-04-02 15:04:00 +0530

    slide4_cumulative_receipt

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def slide4_cumulative_receipt():
         cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)  # Use DictCursor
 
         cursor.execute("""
-            SELECT goods_recipient, amount 
+            SELECT goods_recipient
             FROM slide4_cumulative_receipt
         """)
         results = cursor.fetchall()
@@ -183,7 +183,7 @@ def slide4_cumulative_receipt():
 
         # Now rows behave like dictionaries
         cum_data = [
-            {"goods_recipient": row["goods_recipient"], "amount": row["amount"]} for row in results
+            {"goods_recipient": row["goods_recipient"]} for row in results
         ]
         return jsonify({"Receipt_wise_cumulative_amount": cum_data}), 200
     except Exception as e:
